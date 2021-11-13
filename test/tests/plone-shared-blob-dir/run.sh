ddf5c668558ae9ff966a02fe64d51a0c7de72f9c
Alin Voinea <contact@avoinea.com>
2021-11-13 09:25:54 +0200

    Remove tests overhead

--- test/tests/plone-shared-blob-dir/run.sh
+++ test/tests/plone-shared-blob-dir/run.sh
@@ -15,15 +15,14 @@ zid="$(docker run -d -v $zname:/data --name "$zname" plone/plone-zeo)"
 
 # Start Plone as ZEO Client
 pname="plone-container-$RANDOM-$RANDOM"
-pid1="$(docker run -d -v $zname:/data -e ZEO_SHARED_BLOB_DIR=on --name "${pname}-1" --link=$zname:zeo -e ZEO_ADDRESS=zeo:8100 "$image")"
-pid2="$(docker run -d -v $zname:/data -e ZEO_SHARED_BLOB_DIR=on --name "${pname}-2" --link=$zname:zeo -e ZEO_ADDRESS=zeo:8100 "$image")"
+pid="$(docker run -d -v $zname:/data -e ZEO_SHARED_BLOB_DIR=on --name "${pname}" --link=$zname:zeo -e ZEO_ADDRESS=zeo:8100 "$image")"
 
 # Tear down
-trap "docker rm -vf $pid1 $pid2 $zid > /dev/null" EXIT
+trap "docker rm -vf $pid $zid > /dev/null" EXIT
 
 get() {
 	docker run --rm -i \
-		--link "${pname}-2":plone \
+		--link "${pname}":plone \
 		--entrypoint /app/bin/python \
 		"$image" \
 		-c "from six.moves.urllib.request import urlopen; con = urlopen('$1'); print(con.read())"
@@ -31,7 +30,7 @@ get() {
 
 get_auth() {
 	docker run --rm -i \
-		--link "${pname}-1":plone \
+		--link "${pname}":plone \
 		--entrypoint /app/bin/python \
 		"$image" \
 		-c "from six.moves.urllib.request import urlopen, Request; request = Request('$1'); request.add_header('Authorization', 'Basic $2'); print(urlopen(request).read())"
