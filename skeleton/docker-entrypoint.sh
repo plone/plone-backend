37148dfbf806339e95fd62dc716dbd25966f99fe
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-12-03 08:21:06 +0100

    Allow ADDONS to be present, but empty

--- skeleton/docker-entrypoint.sh
+++ skeleton/docker-entrypoint.sh
@@ -97,7 +97,7 @@ fi
 $sudo $VENVBIN/python /app/scripts/cors.py
 
 # Handle ADDONS installation
-if [[ -v ADDONS ]]; then
+if [[ -z "${ADDONS}" ]]; then
   echo "======================================================================================="
   echo "Installing ADDONS ${ADDONS}"
   echo "THIS IS NOT MEANT TO BE USED IN PRODUCTION"
