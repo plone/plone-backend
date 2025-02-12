81eb95f10e9020195c01a21748cbb08d4068cf4e
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-02-12 15:17:06 +0100

    feat: allow passing CLIENT_HOME as env var

--- skeleton/docker-entrypoint.sh
+++ skeleton/docker-entrypoint.sh
@@ -6,9 +6,16 @@ if [ -z "${PIP_PARAMS}" ]; then
   PIP_PARAMS=""
 fi
 
-# CLIENT HOME
-CLIENT_HOME="/data/$(hostname)/$(hostid)"
-export CLIENT_HOME=$CLIENT_HOME
+echo ${CLIENT_HOME}
+
+if [ -z "${CLIENT_HOME}" ]; then
+  CLIENT_HOME="/app/$(hostname)/$(hostid)"
+  export CLIENT_HOME=$CLIENT_HOME
+fi
+
+
+echo ${CLIENT_HOME}
+
 
 USER="$(id -u)"
 
