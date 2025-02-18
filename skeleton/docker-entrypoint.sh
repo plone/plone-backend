bd88da258f5b95a1d24d98c7aa28e6fab8459f49
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-02-18 08:20:34 +0100

    feat: do not do a find to change ownership of files

--- skeleton/docker-entrypoint.sh
+++ skeleton/docker-entrypoint.sh
@@ -15,7 +15,7 @@ USER="$(id -u)"
 # Create directories to be used by Plone
 mkdir -p /data/filestorage /data/blobstorage /data/cache /data/log $CLIENT_HOME
 if [ "$USER" = '0' ]; then
-  find /data -not -user plone -exec chown plone:plone {} \+
+  # find /data -not -user plone -exec chown plone:plone {} \+
   sudo="gosu plone"
 else
   sudo=""
