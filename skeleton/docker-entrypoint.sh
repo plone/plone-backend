1c8db78969c7c994809b04d9bc8d0d99f56209d8
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-02-28 14:23:33 +0100

    feat: provide packing options

--- skeleton/docker-entrypoint.sh
+++ skeleton/docker-entrypoint.sh
@@ -163,6 +163,14 @@ elif  [[ "$1" == "run" ]]; then
   exec $sudo $VENVBIN/zconsole run etc/${CONF} "${@:2}"
 elif  [[ "$1" == "addzopeuser" ]]; then
   exec $sudo $VENVBIN/addzopeuser -c etc/${CONF} "${@:2}"
+elif  [[ "$1" == "pack" ]]; then
+  if [[ -v ZEO_ADDRESS ]]; then
+    exec $sudo $VENVBIN/zeopack -d "${2:-0}" ${ZEO_ADDRESS}
+  elif [[ -v RELSTORAGE_DSN ]]; then
+    exec $sudo $VENVBIN/zodbpack -d "${2:-0}" etc/relstorage_pack.conf
+  else
+    exec $sudo $VENVBIN/zconsole run etc/${CONF} /app/scripts/pack.py
+  fi
 else
   # Custom
   exec "$@"
