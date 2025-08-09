192f02bea1d2a5d48e8d6a31d283ea8c99c6098f
David Glick <david@glicksoftware.com>
2025-08-09 11:11:41 -0700

    Fix logic for handling existing site
    Fixes #191

--- skeleton/scripts/create_site.py
+++ skeleton/scripts/create_site.py
@@ -90,16 +90,17 @@ logger.info(f"Creating a new Plone site  @ {site_id}")
 logger.info(f" - Using the {DISTRIBUTION} distribution and answers from {answers_file}")
 
 
-if site_id in app.objectIds() and DELETE_EXISTING:
-    app.manage_delObjects([site_id])
-    transaction.commit()
-    app._p_jar.sync()
-    logger.info(f" - Deleted existing site with id {site_id}")
-else:
-    logger.info(
-        f" - Stopping site creation, as there is already a site with id {site_id}. "
-        "Set DELETE_EXISTING=1 to delete the existing site before creating a new one."
-    )
+if site_id in app.objectIds():
+    if DELETE_EXISTING:
+        app.manage_delObjects([site_id])
+        transaction.commit()
+        app._p_jar.sync()
+        logger.info(f" - Deleted existing site with id {site_id}")
+    else:
+        logger.info(
+            f" - Stopping site creation, as there is already a site with id {site_id}. "
+            "Set DELETE_EXISTING=1 to delete the existing site before creating a new one."
+        )
 
 if site_id not in app.objectIds():
     site = site_api._create_site(
