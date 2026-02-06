d07f2c7b7d50c35a750b2bde28c6f28c421da8ed
Érico Andrei <ericof@gmail.com>
2026-02-06 09:24:26 -0300

    Add export to the list of sub-commands available on docker-entrypoint.sh

---
+++ news/+exporter.feature
@@ -0,0 +1,2 @@
+Add `export` to the commands available via docker-entrypoint.sh . @ericof
+  - Usage is ./docker-entrypoint.sh <SITE_ID> <PATH_TO_EXPORT_SITE_DATA>
--- skeleton/docker-entrypoint.sh
+++ skeleton/docker-entrypoint.sh
@@ -159,6 +159,8 @@ elif  [[ "$1" == "create-site" ]]; then
   exec $sudo $VENVBIN/zconsole run etc/${CONF} /app/scripts/create_site.py
 elif  [[ "$1" == "console" ]]; then
   exec $sudo $VENVBIN/zconsole debug etc/${CONF}
+elif  [[ "$1" == "export" ]]; then
+  exec $sudo $VENVBIN/plone-exporter etc/${CONF} "${@:2}" "${@:3}"
 elif  [[ "$1" == "import" ]]; then
   exec $sudo $VENVBIN/plone-importer etc/${CONF} "${@:2}" "${@:3}"
 elif  [[ "$1" == "run" ]]; then
